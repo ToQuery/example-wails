bef7da30ed2941e18649ccdce9fd9938a7063ab1
toquery <toquery@qq.com>
2025-05-08 13:26:14 +0800

    init

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -16,5 +16,5 @@ export const Menus: Menu[] = [
     { text: "首页", path: "/home", icon: "material-symbols:home-app-logo" },
     { text: "用户", path: "/user", icon: "material-symbols:supervisor-account-outline-rounded" },
     { text: "模式", path: "/dark-model", icon: "", footer: true, node: <DarkMode/>, hidden: true },
-    { text: "设置", path: "/setting", icon: "material-symbols:menu-rounded", footer: true },
+    { text: "设置", path: "/setting", icon: "material-symbols:settings", footer: true },
 ];
