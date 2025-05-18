7d3c737299202e9b3991f4eb80f72983db6a63bd
toquery <toquery@qq.com>
2025-05-19 02:18:34 +0800

    init

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import './style.css'
 import App from './App'
 import {ConfigProvider} from "@/provider/config";
