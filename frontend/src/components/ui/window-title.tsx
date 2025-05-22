4de2646baf17c2f6965bdb93da807f5f05dd4f6b
ToQuery <toquery@qq.com>
2025-05-22 23:52:53 +0800

    windows title

--- frontend/src/components/ui/window-title.tsx
+++ frontend/src/components/ui/window-title.tsx
@@ -57,19 +57,19 @@ function WindowTitle(props: WindowTitleProps) {
             {isNotMac ? (
                 <div className="flex justify-end flex-0 text-xl ">
                     <button
-                        className="w-10 flex flex-col items-center justify-center hover:bg-[#E9E9E9] dark:hover:bg-[#2D2D2D] hover:text-gray-500"
+                        className="w-12 flex flex-col items-center justify-center hover:bg-[#E9E9E9] dark:hover:bg-[#2D2D2D] hover:text-gray-500"
                         onClick={() => ExampleService.WebviewWindowMinimize(windowName)}
                     >
                         <Icon icon="mdi:window-minimize"/>
                     </button>
                     <button
-                        className="w-10 flex flex-col items-center justify-center hover:bg-blue-500 hover:text-white dark:hover:bg-[#2D2D2D]"
+                        className="w-12 flex flex-col items-center justify-center hover:bg-blue-500 hover:text-white dark:hover:bg-[#2D2D2D]"
                         onClick={toggleMaximize}
                     >
                         <Icon icon="mdi:window-maximize"/>
                     </button>
                     <button
-                        className="w-10 flex flex-col items-center justify-center hover:bg-red-500 hover:text-white"
+                        className="w-12 flex flex-col items-center justify-center hover:bg-red-500 hover:text-white"
                         onClick={() => ExampleService.WebviewWindowClose(windowName)}
                     >
                         <Icon icon="mdi:close"/>
