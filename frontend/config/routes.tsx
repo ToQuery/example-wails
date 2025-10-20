d3198705e325f3f94b6cefa26cb17f2dddba2c7f
toquery <toquery@qq.com>
2025-10-20 23:41:02 +0800

    重构路由配置，显式声明菜单项的隐藏状态并统一格式

--- frontend/config/routes.tsx
+++ frontend/config/routes.tsx
@@ -24,9 +24,10 @@ export const MainRouters: Menu[] = [
         path: "/",
         page: <IndexPage/>,
         icon: "material-symbols:home-app-logo",
+        hidden: false,
     },
-    {name: 'menu.client', path: "/client", page: <ClientPage/>, icon: "uil:desktop"},
-    {name: 'menu.file', path: "/file", page: <FilePage/>, icon: "material-symbols:files"},
+    {name: 'menu.client', path: "/client", page: <ClientPage/>, icon: "uil:desktop", hidden: true,},
+    {name: 'menu.file', path: "/file", page: <FilePage/>, icon: "material-symbols:files", hidden: true,},
     {
         name: 'menu.example',
         path: "/example",
@@ -43,14 +44,15 @@ export const MainRouters: Menu[] = [
                 path: '/example/2',
                 page: <Example2Page/>,
             },
-        ]
+        ],
+        hidden: false,
     },
-    {name: 'menu.browser', path: "https://github.com/toquery/example-wails", icon: "simple-icons:firefoxbrowser"},
-    {name: 'menu.url', path: "/browser?url=https://github.com/toquery/example-wails", page: <BrowserPage/>, icon: "simple-icons:curl", hidden: true},
-    {name: "menu.dark", render: <ThemeMode/>, footer: true, hidden: false},
-    {name: "menu.language", render: <LanguageSwitcher/>, footer: true, hidden: false},
-    {name: 'menu.sidebar-style', render: <SidebarStyleSwitcher/>, footer: true, hidden: true},
-    {name: 'menu.info', path: "/info", page: <InfoPage/>, icon: "material-symbols:info-outline", footer: true},
+    {name: 'menu.browser', path: "https://github.com/toquery/example-wails", icon: "simple-icons:firefoxbrowser", hidden: false, },
+    {name: 'menu.url', path: "/browser?url=https://github.com/toquery/example-wails", page: <BrowserPage/>, icon: "simple-icons:curl", hidden: true, },
+    {name: "menu.dark", render: <ThemeMode/>, footer: true, hidden: false, },
+    {name: "menu.language", render: <LanguageSwitcher/>, footer: true, hidden: false, },
+    {name: 'menu.sidebar-style', render: <SidebarStyleSwitcher/>, footer: true, hidden: true, },
+    {name: 'menu.info', path: "/info", page: <InfoPage/>, icon: "material-symbols:info-outline", footer: true, hidden: false, },
 ];
 
 export const SettingRouters: Menu[] =  [
@@ -59,6 +61,7 @@ export const SettingRouters: Menu[] =  [
         path: "/setting/general",
         page: <SettingGeneralPage/>,
         icon: "material-symbols:settings",
+        hidden: false,
     },
     {
         name: 'menu.setting.advanced',
@@ -70,19 +73,23 @@ export const SettingRouters: Menu[] =  [
                 path: "/setting/advanced/menu1",
                 page: <SettingAdvancedMenu1/>,
                 icon: "material-symbols:timer-1-rounded",
+                hidden: false,
             },
             {
                 name: 'menu.setting.advanced.menu2',
                 path: "/setting/advanced/menu2",
                 page: <SettingAdvancedMenu2/>,
                 icon: "material-symbols:timer-2-rounded",
+                hidden: false,
             },
         ],
+        hidden: false,
     },
     {
         name: 'menu.setting.info',
         icon: "material-symbols:info-outline",
         path: "/setting/info",
         page: <SettingInfo />,
+        hidden: false,
     },
 ];
